ae04ddde1540bcb60f9c83f69c02d0f2b61964a2
Moritz Hartmeier <moritz@scandit.com>
2022-03-24 11:27:43 +0100

    Add iOS webview integration sample.

---
+++ integration-samples/webviews/ios/ScanditWebViewSample/ViewController.swift
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import WebKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+
+    override func loadView() {
+        let config = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        webView = WKWebView(frame: .zero, configuration: config)
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+
+        switch cameraAuthorizationStatus {
+            case .notDetermined: requestCameraPermission()
+            case .authorized: setupCapture()
+            case .restricted, .denied: alertCameraAccessNeeded()
+        }
+
+        func requestCameraPermission() {
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    setupCapture()
+                }
+            }
+        }
+
+        //access granted
+        func setupCapture() {
+            let url = URL(string:"https://websdk.scandit.com")
+            
+            DispatchQueue.main.async {
+                self.webView.load(URLRequest(url: url!))
+                self.webView.allowsBackForwardNavigationGestures = true
+            }
+        }
+
+        func alertCameraAccessNeeded() {}
+    }
+}
+
